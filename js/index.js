9ea97d8ad4febc08c0a9af51a99fe6698bd64a13
riskezwn <enriquegarciagasc@gmail.com>
2021-03-22 22:35:52 +0100

    bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-bar-reduced").style.top = "0";
+  } else {
+    document.getElementById("nav-bar-reduced").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 function showHideMenu() {
     var x = document.getElementById("links");
     if (x.style.display === "block") {
@@ -26,7 +38,7 @@ function showSlides() {
        slideIndex++;
        if(slideIndex > slides.length) {slideIndex = 1}
        slides[slideIndex-1].style.display = "block";
-       setTimeout(showSlides,8000);
+       setTimeout(showSlides,10000);
 }
 
 function plusSlides() {
